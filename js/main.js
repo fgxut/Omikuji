0810f8abe5b9d72aa979219adf45e32e7c8f5377
fgxut <fgxut1206@gmail.com>
2019-10-28 23:54:29 +0900

    [update]おみくじの確率を変更

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@
     // const results = ['大吉', '中吉', '凶', '末吉'];
     // btn.textContent = results[Math.floor(Math.random() * results.length)];
     const n = Math.random();
-    if (n < 0.05) {
-      btn.textContent = '大吉'; //5%
-    } else if (n < 0.2) {
-      btn.textContent = '中吉'; //15%
+    if (n < 0.1) {
+      btn.textContent = '大吉'; //10%
+    } else if (n < 0.3) {
+      btn.textContent = '中吉'; //30%
     } else {
-      btn.textContent = '凶'; // 80%
+      btn.textContent = '凶'; // 60%
     }
   });
 }
