43f85abfdd2fa0ed1970d6f0ef33d30fb0a03be5
fgxut <fgxut1206@gmail.com>
2019-10-28 23:53:12 +0900

    [fix]スペルミスの修正

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
     if (n < 0.05) {
       btn.textContent = '大吉'; //5%
     } else if (n < 0.2) {
-      btn.taxtContent = '中吉'; //15%
+      btn.textContent = '中吉'; //15%
     } else {
       btn.textContent = '凶'; // 80%
     }
